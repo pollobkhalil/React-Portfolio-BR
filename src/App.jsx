8aade35c643efcaaac0a15186b67a6554e3500f9
Your Name <you@example.com>
2024-06-08 03:19:52 +0600

    added the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import Header from "./components/Header"
 
-function App() {
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   
+    <>
+    
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+
+    <Header/>
+
+    </div>
+    
+    </>
+
+
   )
 }
 
